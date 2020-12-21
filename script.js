3e0898e5f224099e6fb0b96e6f1ab6e1e936bfda
Ivano Monego <52533028+monego-ivano@users.noreply.github.com>
2020-12-21 21:17:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let counter = document.querySelector('.par');
+const minusBtn = document.querySelector('#minus');
+const plusBtn = document.querySelector('.plus');
+const resetBtn = document.querySelector('.reset-btn');
+const btns = document.querySelectorAll('.btn');
+
+minusBtn.disabled = true;
+
+plusBtn.addEventListener('click', function () {
+    counter.textContent = Number(counter.textContent) + 1;
+});
+
+minusBtn.addEventListener('click', function () {
+    counter.textContent = Number(counter.textContent) - 1;
+});
+
+resetBtn.addEventListener('click', () => {
+    counter.textContent = 0
+    minusBtn.disabled = true;
+});
+
+plusBtn.addEventListener('click', function () {
+    if (counter.textContent == 0) {
+        minusBtn.disabled = true;
+    } else {
+        minusBtn.disabled = false;
+    }
+});
+
+minusBtn.addEventListener('click', function () {
+    if (counter.textContent == 0) {
+        minusBtn.disabled = true;
+    } else {
+        minusBtn.disabled = false;
+    }
+});
